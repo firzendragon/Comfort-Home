ea9611060dd0918d3655adc6ad545367d53de4fd
firzendragon <firzendragon@gmail.com>
2012-03-01 23:53:36 +0800

    Version 1.3

--- optimization/setting.py
+++ optimization/setting.py
@@ -1,10 +1,12 @@
+import time
+
 def getInstructions(population, actuators, maxNumActuators):
     pass
 
 def printSection(population, actuators, maxNumActuators):
     print '----------'
+    timeNow = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+    print timeNow
     print population.score, population.numOfGeneration
     for k in range(maxNumActuators):
         print population.actuatorPlan[k], actuators[k].name
-
-        
